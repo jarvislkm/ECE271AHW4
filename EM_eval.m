4730af29d8dead99ce00690686c112ef15f368ec
linux5 <kal251@eng.ucsd.edu>
2018-11-30 08:50:47 -0800

    change error

--- EM_eval.m
+++ EM_eval.m
@@ -13,8 +13,8 @@ p_mu = p_mu(:,1:dim);
 p_var = p_var(:,1:dim,1:dim);
 
 for j = 1:c
-    likelihood = gaussian_likelihood(data, p_mu(j,:), p_var(j,:,:));
-    likelihood = likelihood + likelihood.* p_weight(j);
+    likelihood_ = gaussian_likelihood(data, p_mu(j,:), p_var(j,:,:));
+    likelihood = likelihood + likelihood_.* p_weight(j);
 end
 end
 
