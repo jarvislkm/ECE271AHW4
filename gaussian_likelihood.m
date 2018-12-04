1aa8291fc7c896aac3c4b4212cc7912ee51beb04
Kaiming Jam <git@github.com:jarvislkm>
2018-12-03 19:45:03 -0800

    minor change

--- gaussian_likelihood.m
+++ gaussian_likelihood.m
@@ -6,9 +6,10 @@ function [likelihood] = gaussian_likelihood(data, mu, var)
     likelihood = zeros(size(data,1),1);
     tmp_var = squeeze(var);
     tmp_var_inv = inv(tmp_var);
+    tmp_var_det = (det(tmp_var))^(0.5);
     for i = 1:size(data, 1)
         tmp_x = data(i,:)-mu;
-        likelihood(i) = 1/((det(tmp_var))^(0.5)) * exp(-0.5*tmp_x*tmp_var_inv*tmp_x');
+        likelihood(i) = 1/tmp_var_det * exp(-0.5*tmp_x*tmp_var_inv*tmp_x');
     end
 end
 
